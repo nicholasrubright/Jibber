35cf58db665ff2e052f1abb0220b831dbf159147
DESKTOP-GRUEFOB\koalr <koalrube@gmail.com>
2023-06-21 19:00:05 -0400

    added css and removed stuff i don't need

--- jibber-app/src/pages/_app.tsx
+++ jibber-app/src/pages/_app.tsx
@@ -1,6 +1,9 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "bootstrap/dist/css/bootstrap.css";
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
